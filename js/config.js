4c5537405b83a97d6474c91dcfdabdc879a2b73e
safainfo-app <appettore@gmail.com>
2025-05-21 15:28:43 +0200

    Update sheets link

--- js/config.js
+++ js/config.js
@@ -11,8 +11,8 @@ const config = {
     customPageField: "entry.2143033031",
     parentField: "entry.53127251",
     categoryField: "entry.1718811362",
-    pagesTable: "https://docs.google.com/spreadsheets/d/1ZuUVyBmIU_Ax5V7Iq-yCUQvN-tmF8RZEh1uhkT6HVFA/export?format=csv",
-    deletedTable: "https://docs.google.com/spreadsheets/d/1FVN90zGMNJbKOiBJWCVdVH7UFI74yny4G-3vJBzwrEo/export?format=csv",
+    pagesTable: "https://docs.google.com/spreadsheets/d/1SWsSN6AtrsibtLpXBujNytp6fXkicCPO4IzvKYsaT3c/export?format=csv",
+    deletedTable: "https://docs.google.com/spreadsheets/d/11Rm1TlXhd83oFcDuLr_S58N_6utNPzZh8QfjTXf_jZc/export?format=csv",
     deletionEntryId: 'entry.1372900842',
     creationFormIdHsh: '822cefd8fbee2e592919a29708bab929a84d1ce0', //SHA1 Hash
     deletionFormIdHash: 'fab33b18a44810eae7bba397c049b7de514894a5' //SHA1 Hash

92bc30f7ff3a6481958e1dba383250cb6f1f8b6b
safainfo-app <appettore@gmail.com>
2026-01-14 20:59:44 +0100

    Update service worker cache version

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'safa-cache-v78';
+const CACHE_NAME = 'safa-cache-v79';
 const EXCLUDE_FROM_CACHE = [
     "https://docs.google.com/spreadsheets/d/1SWsSN6AtrsibtLpXBujNytp6fXkicCPO4IzvKYsaT3c/export?format=csv",
     "https://docs.google.com/spreadsheets/d/11Rm1TlXhd83oFcDuLr_S58N_6utNPzZh8QfjTXf_jZc/export?format=csv"
@@ -140,3 +140,4 @@ self.addEventListener('message', (event) => {
         });
     }
 });
+
